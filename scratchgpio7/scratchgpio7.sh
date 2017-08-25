3afcd73e0373bc868b01dbe037d48092a4168623
Ecce_Li <Ecce_Li@asus.com>
2017-08-25 18:52:42 +0800

    Remove '\r' so that the Desktops's shortcut run correctly.
    Change-Id: I498e9537e39f078edb3e4778fa3f847461838adf

--- scratchgpio7/scratchgpio7.sh
+++ scratchgpio7/scratchgpio7.sh
@@ -5,5 +5,5 @@
 #Version 1.0 - 29Oct13 sw - change to cd into simplesi_scratch_handler to run servods OK
 sudo pkill -f scratchgpio_handler
 cd /scratchgpio7
-sudo python scratchgpio_handler7.py 127.0.0.1 standard &
+sudo python scratchgpio_handler7.py 127.0.0.1 standard  &
 scratch --document "/home/linaro/Documents/Scratch Projects/GPIO_IN_OUT_TEST.sb" &
